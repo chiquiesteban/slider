ec92434b5aebe9b932c7dc560e734f91180730d2
Chiqui <chiquiesteban@gmail.com>
2013-04-10 13:49:34 -0300

    Create utils.js
    This is the only file you would need to change for basic purposes.

---
+++ utils.js
@@ -0,0 +1,17 @@
+var ratio = 0.75;
+/*ASPECT RATIO OF THE IMAGES LOADED --- IF THEY ARE 800px * 600 px, THE RATIO IS 0.75, FOR 1000px * 1000px ratio=1*/
+
+var data = [
+  ["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/0.jpg"],
+	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/10.jpg"],
+	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/20.jpg"],
+	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/30.jpg"],
+	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/40.jpg"],
+	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/50.jpg"],
+	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/60.jpg"],
+	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/70.jpg"],
+	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/80.jpg"],
+	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/90.jpg"],
+	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/100.jpg"]
+];
+/*INCLUDE AS MANY IMAGES AS YOU WANT TO*/
