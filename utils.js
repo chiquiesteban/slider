f5b61301fface85c285db3006065e7ac8abb2c03
Chiqui <chiquiesteban@gmail.com>
2013-04-17 15:00:32 -0300

    Update utils.js
    Adding feature to include text for each slider

--- utils.js
+++ utils.js
@@ -2,16 +2,16 @@ var ratio = 0.75;
 /*ASPECT RATIO OF THE IMAGES LOADED --- IF THEY ARE 800px * 600 px, THE RATIO IS 0.75, FOR 1000px * 1000px ratio=1*/
 
 var data = [
-  ["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/0.jpg"],
-	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/10.jpg"],
-	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/20.jpg"],
-	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/30.jpg"],
-	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/40.jpg"],
-	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/50.jpg"],
-	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/60.jpg"],
-	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/70.jpg"],
-	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/80.jpg"],
-	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/90.jpg"],
-	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/100.jpg"]
+  	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/0.jpg", "Text 0"],
+	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/10.jpg", "Text 1"],
+	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/20.jpg", "Text 2"],
+	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/30.jpg", "Text 3"],
+	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/40.jpg", "Text 4"],
+	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/50.jpg", "Text 5"],
+	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/60.jpg", "Text 6"],
+	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/70.jpg", "Text 7"],
+	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/80.jpg", "Text 8"],
+	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/90.jpg", "Text 9"],
+	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/100.jpg", "Text 10"]
 ];
 /*INCLUDE AS MANY IMAGES AS YOU WANT TO*/
